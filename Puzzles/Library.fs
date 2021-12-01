deb6c1ec902a00267297a9dc56743606533b353f
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2021-12-01 15:28:49 +0100

    initial commit

---
+++ Puzzles/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace Puzzles
+
+module Day1 =
+    let solve_part1 input =
+        
+        input
+            |> Seq.pairwise
+            |> Seq.where (fun (a, b) -> b > a)
+            |> Seq.length
+
+    let solve_part2 (input: seq<int>) =
+        input
+            |> Seq.windowed 3
+            |> Seq.pairwise
+            |> Seq.where (fun (a, b) -> (Seq.sum b) > (Seq.sum a))
+            |> Seq.length
