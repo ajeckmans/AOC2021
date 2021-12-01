fe3b0c96da4ca4c5eec3dffc0908ae03006b7cd9
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2021-12-01 15:44:07 +0100

    Generalize solution

--- Puzzles/Library.fs
+++ Puzzles/Library.fs
@@ -1,16 +1,17 @@
 ﻿namespace Puzzles
 
 module Day1 =
+    let countIncreasing map input =
+        input
+            |> Seq.pairwise
+            |> Seq.where (fun (a, b) -> map b > map a)
+            |> Seq.length
+    
     let solve_part1 input =
-        
         input
-            |> Seq.pairwise
-            |> Seq.where (fun (a, b) -> b > a)
-            |> Seq.length
+        |> countIncreasing id
 
-    let solve_part2 (input: seq<int>) =
+    let solve_part2 input =
         input
-            |> Seq.windowed 3
-            |> Seq.pairwise
-            |> Seq.where (fun (a, b) -> (Seq.sum b) > (Seq.sum a))
-            |> Seq.length
+        |> Seq.windowed 3
+        |> countIncreasing (Seq.reduce (+))
