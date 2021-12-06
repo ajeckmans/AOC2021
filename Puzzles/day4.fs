6c4cd4e14bdb0e704681d85507f68a0d5230d12a
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2021-12-06 15:33:36 +0100

    Clean up day 4 a bit

--- Puzzles/day4.fs
+++ Puzzles/day4.fs
@@ -6,12 +6,6 @@ open FSharpPlus.Data
 open Puzzles
 
 module Day4 =    
-    type Number =
-        | Called of int
-        | Uncalled of int
-
-    type Card = { Numbers: Number[,] }
-        
     let input =
         use stream = inputs.GetResourceStream("day4.txt")
         use reader = new StreamReader(stream)
@@ -19,52 +13,49 @@ module Day4 =
         let numbers =
             reader.ReadLine()
             |> fun s -> s.Split(',')
-            |> Seq.map int
             |> Array.ofSeq
 
         let cards = 
             seq {
                 while not reader.EndOfStream do
                     reader.ReadLine() |> ignore
-                    let numbers =  Array2D.create 5 5 (Uncalled 0)
+                    let numbers =  Array2D.create 5 5 ""
                     for i in 0 .. 4 do
-                        let e1, e2, e3, e4, e5 =
+                        numbers.[i,*] <-
                             reader.ReadLine()
                             |> String.replace "  " " "
                             |> String.trimWhiteSpaces
-                            |> sscanf "%i %i %i %i %i"
-                        numbers.[i,*] <- [| Uncalled e1; Uncalled e2; Uncalled e3; Uncalled e4; Uncalled e5|]
+                            |> String.split [ " " ]
+                            |> Array.ofSeq
                         
-                    yield { Numbers = numbers }
+                    yield numbers
             } |> List.ofSeq
         
         numbers, cards
 
     let updateCard number card =
-        { card with Numbers = Array2D.map (fun x ->
-                                 match x with
-                                 | Uncalled x when x = number -> Called x
-                                 | Uncalled x -> Uncalled x
-                                 | Called x -> Called x ) card.Numbers
-        }
+         Array2D.map (fun x ->
+             match x with
+             | x when x = number -> "x"
+             | _ -> x) card
         
-    let cardIsWinner card =
+    let cardIsWinner (card: string[,]) =
         [0 .. 4]
         |> Seq.exists (fun i ->
-            card.Numbers.[i,*] |> Seq.forall (fun x -> match x with | Called _ -> true | _ -> false )
-            || card.Numbers.[*,i] |> Seq.forall (fun x -> match x with | Called _ -> true | _ -> false )
+            card.[i,*] |> Seq.forall (fun x -> x = "x" )
+            || card.[*,i] |> Seq.forall (fun x -> x = "x" )
         )
         
     let calculateCardScore currentNumber card =
         let sumOfUnmarked =
-            card.Numbers
-            |> Seq.cast<Number>
-            |> Seq.where (fun x -> match x with | Uncalled _ -> true | _ -> false )
-            |> Seq.sumBy (fun x -> match x with | Uncalled x -> x | _ -> 0 )
+            card
+            |> Seq.cast<string>
+            |> Seq.where (fun x -> x <> "x" )
+            |> Seq.sumBy int
         
-        sumOfUnmarked * currentNumber
+        sumOfUnmarked * (int currentNumber)
         
-    let solve_1 =      
+    let solve_1 (input: string[] * string[,] list) =      
         let numbers = input |> fst
         
         seq {
@@ -85,7 +76,7 @@ module Day4 =
                 step <- step + 1
         } |> Seq.head
         
-    let solve_2 =      
+    let solve_2 (input: string[] * string[,] list) =      
         let numbers = input |> fst
         
         seq {
@@ -97,9 +88,9 @@ module Day4 =
                 let currentNumber = numbers.[step]
                 let newCards, changed =
                     cards
-                    |> List.mapFold (fun (state: Card list) elem ->
+                    |> List.mapFold (fun state elem ->
                         let newCard = updateCard currentNumber elem
-                        if newCard.Numbers <> elem.Numbers && cardIsWinner elem = false && cardIsWinner newCard then
+                        if newCard <> elem && cardIsWinner elem = false && cardIsWinner newCard then
                             newCard, state @ [newCard]
                         else
                             newCard, state
--- Puzzles/day4.tests.fs
+++ Puzzles/day4.tests.fs
@@ -8,11 +8,11 @@ type ``Day 4 Tests``() =
    
     [<Fact>]
     let ``part1: actual input`` () =
-        let result = solve_1
+        let result = solve_1 input
         Assert.Equal(51034, result)
         
         
     [<Fact>]
     let ``part2: actual input`` () =
-        let result = solve_2
+        let result = solve_2 input
         Assert.Equal(5434, result)
