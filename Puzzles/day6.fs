1a7f3ecadf3d0c5281d1d56916fe62ac13446a9b
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2021-12-06 21:05:52 +0100

    Simplify

--- Puzzles/day6.fs
+++ Puzzles/day6.fs
@@ -12,19 +12,15 @@ module Day6 =
         reader.ReadLine()
             |> String.split [","]
             |> Seq.map int
-            |> Seq.groupBy id
-            |> Seq.sortBy fst
-            |> Seq.map (fun (_, list) -> length list |> int64)
-            |> List.ofSeq
-            |> (fun x -> (x @ [0L;0L;0L;0L;0L;0L;0L;0L;0L]).[0..8])
+            |> Seq.fold (fun state x -> state |> List.updateAt x (state.[x] + 1L)) (List.init 9 (fun _ -> 0L))
 
+    let updateValueAt index valueFunc list = List.updateAt index (valueFunc (list |> List.item index)) list
+        
     let run runs input =
-        [1 .. runs - 1]
-        |> Seq.fold (fun state _ -> List.map2 (+) (state.[1..] @ [0L]) [0L;0L;0L;0L;0L;0L;state.[0];0L;state.[0]]) input
+        [1 .. runs]
+        |> Seq.fold (fun (state: int64 list) _ -> (state.[1..] |> updateValueAt 6 ((+) state.[0])) @ [state.[0]]) input
         |> Seq.sum
         
-    let solve_1 input =
-        run 80 input
+    let solve_1 input = run 80 input
         
-    let solve_2 input = 
-        run 256 input
+    let solve_2 input = run 256 input
