62f71e7eb95ba37dd660996f860c7e6cd25efab5
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2021-12-07 10:19:53 +0100

    Use median for first part

--- Puzzles/day7.fs
+++ Puzzles/day7.fs
@@ -14,15 +14,18 @@ module Day7 =
             |> String.split [","]
             |> Seq.map int64
 
-    let rec find list lowest position costFunction : int64 =
-        match list |> Seq.map (fun x -> (costFunction x position)) |> Seq.sum with
-        | cost when cost > lowest -> lowest
-        | cost when cost < lowest -> find list cost (position+1L) costFunction
-        | _ -> find list lowest (position+1L) costFunction
-        
-    let solve_1 input =
-        find input Int64.MaxValue 0L (fun x position -> x - position |> abs)
+    let solve_1 (input: seq<int64>) =
+        let sorted = input |> sort |> Array.ofSeq
+        let median = sorted.[(sorted |> length) / 2]
+        sorted |> Seq.map (fun x -> (x - median) |> abs) |> Seq.sum
         
     let solve_2 input =
         let cost n = if n = 0L then 0L else [1L..n] |> List.reduce (+)
-        find input Int64.MaxValue 0L (fun x position -> cost (x - position |> abs))
+        
+        let rec find list lowest position : int64 =
+            match list |> Seq.map (fun x -> cost (x - position |> abs)) |> Seq.sum with
+            | cost when cost > lowest -> lowest
+            | cost when cost < lowest -> find list cost (position+1L)
+            | _ -> find list lowest (position+1L)
+        
+        find input Int64.MaxValue 0L
